14c04f4230fa01f47df95a58f49865e1e5f1f353
DamianPerdigon <perdigonalvarezdamian@gmail.com>
2023-10-25 19:17:34 +0100

    adding states

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -1,19 +1,17 @@
-// Importing React library for building the component and the CSS file for styling
 import React from 'react';
 import './Hangman.css';
 
-// Defining the Hangman functional component
-// This component is responsible for displaying the hangman graphic and the number of attempts left.
 function Hangman({ attempts }) {
+  const totalAttempts = 10; // 10 attempts
+  const hangmanState = totalAttempts - attempts + 1; // +1 because are 11 states
+  const hangmanImage = `${process.env.PUBLIC_URL}/images/state${hangmanState}.gif`;
+
   return (
-    // The hangman graphic and attempts information are wrapped in a div with a class for styling.
     <div className="hangman">
-      {/* Displaying a text representation of the hangman game and the number of attempts left.
-           This provides a visual cue to the player about their remaining chances. */}
-      <p>Hangman - Attempts left: {attempts}</p>
+      <img src={hangmanImage} alt={`Hangman State ${hangmanState}`} />
+      <p>Attempts left: {attempts}</p>
     </div>
   );
 }
 
-// Exporting the Hangman component to be used in other parts of the application.
 export default Hangman;
